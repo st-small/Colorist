ee226b2d26716c34780c5fd16c1113f4a7ecfc5d
Станислав Шияновский <st-small@rambler.ru>
2019-10-18 14:32:37 +0300

    Carthage init.

---
+++ Carthage/Build/iOS/Colorist.framework/Headers/Colorist.h
@@ -0,0 +1,19 @@
+//
+//  Colorist.h
+//  Colorist
+//
+//  Created by Станислав Шияновский on 10/9/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Colorist.
+FOUNDATION_EXPORT double ColoristVersionNumber;
+
+//! Project version string for Colorist.
+FOUNDATION_EXPORT const unsigned char ColoristVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Colorist/PublicHeader.h>
+
+
---
+++ Colorist/Worker.swift
@@ -0,0 +1,18 @@
+//
+//  Worker.swift
+//  Colorist
+//
+//  Created by Станислав Шияновский on 10/9/19.
+//  Copyright © 2019 Станислав Шияновский. All rights reserved.
+//
+
+import UIKit
+
+@objc public class Worker: NSObject {
+
+    @objc public func makeColor() -> UIColor {
+        return UIColor(red: CGFloat.random(in: 0..<1),
+                       green: CGFloat.random(in: 0..<1),
+                       blue: CGFloat.random(in: 0..<1), alpha: 1.0)
+    }
+}
